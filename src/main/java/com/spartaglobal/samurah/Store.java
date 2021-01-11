f621ef256c5b3348e249d253b6e951fa5e0f696b
Andrei Pavel <samurahh@gmail.com>
2021-01-11 18:34:18 +0000

    Merged Development with Main. Merging errors fixed.

--- src/main/java/com/spartaglobal/samurah/Store.java
+++ src/main/java/com/spartaglobal/samurah/Store.java
@@ -1,9 +1,16 @@
 package com.spartaglobal.samurah;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
-public class Store implements StoreInterface{
+public class Store implements StoreInterface {
+    private final HashMap<String, Integer> products;
+
+    public Store() {
+        products = new HashMap<>();
+    }
+
     @Override
     public int getCost(String product) {
         return 0;
@@ -16,17 +23,29 @@ public class Store implements StoreInterface{
 
     @Override
     public boolean addProduct(String productName, int price) {
-        return false;
+        if (productName == null || doesItemExist(productName)) {
+            return false;
+        } else if (price > 0) {
+            products.put(productName, price);
+            return true;
+        } else {
+            System.out.println("Price must be greater than zero.");
+            return false;
+        }
     }
 
     @Override
     public boolean removeProduct(String productName) {
-        return false;
+        if (productName != null) {
+            products.remove(productName);
+            return true;
+        }
+        else return false;
     }
 
     @Override
     public Map<String, Integer> getItems() {
-        return null;
+        return products;
     }
 
     @Override
@@ -38,9 +57,15 @@ public class Store implements StoreInterface{
     public boolean addProducts(Map<String, Integer> products) {
         return false;
     }
-
     @Override
     public boolean removeProducts(Collection<String> products) {
-        return false;
+        for (String product : products) {
+            if (doesItemExist(product)) {
+                removeProduct(product);
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 }
