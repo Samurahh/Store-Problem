2542bac2d5c62e0d52cba3615568cc4b9ed14d09
Jamie Hammond <33955664+jamiehammond@users.noreply.github.com>
2021-01-11 15:57:26 +0000

    Completed initialize and getShoppingList methods.

---
+++ src/main/java/com/spartaglobal/samurah/FileReader.java
@@ -0,0 +1,43 @@
+package com.spartaglobal.samurah;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FileReader {
+
+    private BufferedReader reader;
+
+    public ArrayList<String> getShoppingList() {
+        ArrayList<String> items = new ArrayList<>();
+        String line;
+        try {
+            while (((line = reader.readLine()) != null)) {
+                String[] splitLine = line.split(",");
+                items.addAll(Arrays.asList(splitLine));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return items;
+    }
+
+    public boolean initialize(String filePath) {
+        File file = new File(filePath);
+        if (file.exists()) {
+            try {
+                reader = new BufferedReader(new java.io.FileReader(file));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                return false;
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
