80a16d114a87ee4482c248dd4cedaa6be09ed8eb
Andrei Pavel <samurahh@gmail.com>
2021-01-11 18:23:01 +0000

    Created tests for Store.java and ShoppingListReader.java

---
+++ src/main/java/com/spartaglobal/samurah/ShoppingListReader.java
@@ -0,0 +1,15 @@
+package com.spartaglobal.samurah;
+
+import java.util.Collection;
+
+public class ShoppingListReader implements ShoppingListReaderInterface {
+    @Override
+    public Collection<String> getShoppingList() {
+        return null;
+    }
+
+    @Override
+    public boolean initialize(String filePath) {
+        return false;
+    }
+}
